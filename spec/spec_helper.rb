588cebd41d87b7b1aefe2069b98b8f42638f5607
Luca Spiller <luca@stackednotion.com>
2011-04-18 14:02:26 +0100

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'texty'
+require 'rspec/core'
+require 'rspec/expectations'
+require 'rspec/mocks'
+
+module Rspec
+  module Core
+    module Matchers
+      def fail
+        raise_error(::Rspec::Expectations::ExpectationNotMetError)
+      end
+
+      def fail_with(message)
+        raise_error(::Rspec::Expectations::ExpectationNotMetError, message)
+      end
+    end
+  end
+end
+
+Rspec.configure do |c|
+end
